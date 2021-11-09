28bfb158fe019374117ec2a5e22c655509efe249
Paul <liquidmonkey@gmail.com>
2021-11-09 02:19:18 -0700

    switch to yarn serve (from yarn run serve) in Procfile semicolons, spacing

--- tools/copy.js
+++ tools/copy.js
@@ -1,24 +1,15 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
-import path from 'path';
-import chokidar from 'chokidar';
-import { writeFile, copyFile, makeDir, copyDir, cleanDir } from './lib/fs';
-import pkg from '../package.json';
-import { format } from './run';
+import path from 'path'
+import chokidar from 'chokidar'
+import {cleanDir, copyDir, copyFile, makeDir, writeFile} from './lib/fs'
+import pkg from '../package.json'
+import {format} from './run'
 
 /**
  * Copies static files such as robots.txt, favicon.ico to the
  * output (build) folder.
  */
 async function copy() {
-  await makeDir('build');
+  await makeDir('build')
   await Promise.all([
     writeFile(
       'build/package.json',
@@ -37,6 +28,7 @@ async function copy() {
     ),
     copyFile('LICENSE.txt', 'build/LICENSE.txt'),
     copyFile('yarn.lock', 'build/yarn.lock'),
+    copyFile('Procfile', 'build/Procfile'),
     copyDir('public', 'build/public'),
   ]);
 
